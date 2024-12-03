7e9f16695afdbfc251e35804a38f1b43bea9dc4b
femke77 <megan.meyers.388@gmail.com>
2024-12-02 22:13:28 -0800

    updated to async await

--- seed.js
+++ seed.js
@@ -4,31 +4,37 @@ const fetch = require("node-fetch");
 const Movie = require("./models/Movie");
 const apiKey = process.env.API_KEY;
 
-connection.on("error", (err) => err);
+connection.on("error", (err) => console.error(err.message));
 
 const movies = [];
 
-fetch(`https://api.themoviedb.org/3/movie/now_playing/?api_key=${apiKey}`)
-  .then((response) => response.json())
-  .then((response) => {
-    response.results.forEach((m) => {
-      const nextMovie = {
-        title: m.title,
-        poster_path: m.poster_path,
-      };
-      movies.push(nextMovie);
-    });
-  })
-  .catch((err) => console.error(err));
+const fetchData = async () => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/now_playing/?api_key=${apiKey}`
+  );
+  const response = await res.json();
+  response.results.forEach((m) => {
+    const nextMovie = {
+      title: m.title,
+      poster_path: m.poster_path,
+    };
+    movies.push(nextMovie);
+  });
+  console.log("Got movies from API. Ready to insert into DB.");
+};
 
 connection.once("open", async () => {
-  console.log("Connected to db.");
   try {
+    console.log("Connected to db.");
+    await fetchData();
     await Movie.deleteMany({});
     await Movie.insertMany(movies);
-    console.info("Seeding complete! 🌱");
+    console.info(
+      "Seeding complete! 🌱 Seeding complete! 🌱 Seeding complete! 🌱"
+    );
     process.exit(0);
-  } catch (error) {
-    console.log(error.message);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
   }
 });
